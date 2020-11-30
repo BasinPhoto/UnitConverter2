b103fa3b734f8a658cc4bde1b11a4141193214a2
BasinS <shoom.pozitiv@gmail.com>
2020-11-30 21:09:24 +0200

    Initial Commit

---
+++ UnitConverter2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitConverter2
+//
+//  Created by Sergey Basin on 30.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UnitConverter2/UnitConverter2App.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverter2App.swift
+//  UnitConverter2
+//
+//  Created by Sergey Basin on 30.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverter2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
