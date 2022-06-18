aaf7acc6f2aa9b19089284cb465c7422250c9ed5
Serg Basin <shoom.pozitiv@gmail.com>
2022-06-18 23:05:34 +0300

    go on

---
+++ UnitConverter2/ViewModel/ViewModel.swift
@@ -0,0 +1,110 @@
+//
+//  ViewModel.swift
+//  UnitConverter2
+//
+//  Created by Sergey Basin on 30.11.2020.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+
+class ViewModel: ObservableObject {
+    private var subscriptions = Set<AnyCancellable>()
+    @Published var leftToRight: Bool = true
+    @Published var selection1: String = ""
+    @Published var selection2: String = ""
+    
+    @Published var inputValue: String = "0"
+    @Published var resultValue: Double = 0
+    
+    @Published var type: UnitType = .time {
+        didSet {
+            selection1 = ""
+            selection2 = ""
+        }
+    }
+    
+    var labels: [String] {
+        type.labels
+    }
+    
+    func getCurrencies() {
+        NetworkManager.fetchData(urlAPI: NetworkManager.urlAPI) { (requestResult) in
+            DispatchQueue.main.async {
+                if let fetchingResult = requestResult {
+                    UnitType.allValues.append(fetchingResult.conversionRates)
+                }
+            }
+        }
+    }
+    
+    private var valuesDictionary: [String : Double] {
+        return UnitType.allValues[type.rawValue]
+    }
+    
+    private var keysArray: [String] {
+        valuesDictionary.keys.sorted()
+    }
+    
+    private func calc() {
+        var returnedRsult: Double = 0
+        
+        if let value = Double(self.inputValue) {
+            guard let valueFrom = self.valuesDictionary[self.selection1] else {
+                return
+            }
+            guard let valueTo = self.valuesDictionary[self.selection2] else {
+                return
+            }
+            
+            switch self.type {
+            case .money:
+                returnedRsult = (value * valueTo / valueFrom)
+            default:
+                returnedRsult =  (value * valueFrom / valueTo)
+            }
+            
+            self.resultValue = returnedRsult
+        }
+    }
+    
+    init() {
+        $inputValue
+            .receive(on: RunLoop.main)
+            .sink { _ in
+                self.calc()
+            }
+            .store(in: &subscriptions)
+        
+        $type
+            .receive(on: RunLoop.main)
+            .sink { _ in
+                self.calc()
+            }
+            .store(in: &subscriptions)
+        
+        $selection1
+            .receive(on: RunLoop.main)
+            .sink { _ in
+                self.calc()
+            }
+            .store(in: &subscriptions)
+        
+        $selection2
+            .receive(on: RunLoop.main)
+            .sink { _ in
+                self.calc()
+            }
+            .store(in: &subscriptions)
+        
+        $leftToRight
+            .receive(on: RunLoop.main)
+            .sink { _ in
+                let tmp = self.selection1
+                self.selection1 = self.selection2
+                self.selection2 = tmp
+            }
+            .store(in: &subscriptions)
+    }
+}
